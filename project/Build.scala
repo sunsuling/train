025bc5b42c453f058fe488e7c3fb071787775b92
sunsuling <sunsuling@163.com>
2014-11-01 13:12:15 +0800

    项目提交

--- project/Build.scala
+++ project/Build.scala
@@ -9,13 +9,20 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    jdbc,
-    anorm
+     javaCore,
+	  javaJdbc,
+	  javaEbean,
+	  "com.github.mumoshu" %% "play2-memcached" % "0.3.0.2",
+	  //"mchv" %% "play2-quartz" % "1.1",
+	  "jaxen" % "jaxen" % "1.1.4",
+	  "javax.mail" % "mail" % "1.4.7",
+	  "jp.furyu" %% "play-c3p0-plugin" % "0.2.1"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here 
+    resolvers += "Spy Repository" at "http://files.couchbase.com/maven2"     
   )
 
 }
